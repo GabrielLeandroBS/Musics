be6ff61acb50a284eb31919c2adfc63108477462
Gabriel Leandro <48261292+GabrielLeandroBS@users.noreply.github.com>
2022-05-15 23:32:22 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
+const { resolve } = require('path')
+const { defineConfig } = require('vite')
 import react from '@vitejs/plugin-react'
+import { splitVendorChunkPlugin } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react(), splitVendorChunkPlugin()],
+  raw: {
+    extenstions: ['html', 'txt'],
+    glob: ['**.html'] // or glob
+  },
+  build: {
+    minify: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      }
+    }
+  }
 })
